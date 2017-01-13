5f86e467a83471953124740d0e1b6ab31dc4aeb0
Codepressd <creederone@gmail.com>
2017-01-13 09:57:42 -0800

    another minor tweak on server

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ if (require.main === module) {
     });
 }
 
-// app.get('/', (req, res) => {
-//     res.send('The server is running');
-// });
+app.get('/', (req, res) => {
+    res.send('The server is running');
+});
 
 //express setup
 
